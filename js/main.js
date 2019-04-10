fd3fabd4d7e2332b6420484ff0e3f63bfd02db8d
tuyengabi <tuyen.gabi@gmail.com>
2019-04-10 23:06:16 +0200

    edit button add maxscore and reset

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 "use strict";
 
-let src = document.getElementsByTabName('input');
-console.log(src);
+//function add Input to Max New Score
+let valueInput = document.getElementById('score').value;
+console.log(valueInput);
+let newSrc = document.getElementById('maxscore');
+
+function maxScore() {
+  newSrc.innerHTML = valueInput;
+}
+
+//function reset
+
+function resetScore() {
+  location.reload();
+  document.getElementById('score').value = '';
+}
 
 
