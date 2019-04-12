c5d6679057b04cb44993bbf3e823f755ba0328c9
tuyengabi <tuyen.gabi@gmail.com>
2019-04-12 14:19:25 +0200

    add function freeze

--- js/script.js
+++ js/script.js
@@ -1,15 +1,30 @@
 "use strict";
 
-let scorePlayer1 = document.getElementsByClassName('scorePlayer1')[0].innerHTML;
-let scorePlayer2 = document.getElementsByClassName('scorePlayer2')[0].innerHTML;
-let btnPlayer1 = document.getElementsByClassName('btnPlayer1')[0];
-let btnPlayer2 = document.getElementsByClassName('btnPlayer2')[0];
-let btnReset = document.getElementsByClassName('btnReset')[0];
-
+const scorePlayer1 = document.getElementsByClassName('scorePlayer1')[0].innerHTML;
+const scorePlayer2 = document.getElementsByClassName('scorePlayer2')[0].innerHTML;
+const btnPlayer1 = document.getElementsByClassName('btnPlayer1')[0];
+const btnPlayer2 = document.getElementsByClassName('btnPlayer2')[0];
+const btnReset = document.getElementsByClassName('btnReset')[0];
 let score1 = 0;
 let score2 = 0;
+let max = 5;
 
-let oui = 0;
-function scoreIncrement() {
-  return ++score1;
+function freeze() {
+  btnPlayer1.disabled = true;
+  btnPlayer2.disabled = true;
+}
+function scoreIncrement1() {
+  if (score1 == max) {
+    return;
+  } else {
+  score1++;
+  }
+}
+
+function scoreIncrement2() {
+  if (score2 == max) {
+    return;
+  } else {
+  score2++;
+  }
 }
