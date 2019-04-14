76b219e42c50251794bdaac0be1f3f0d7414ef16
tuyengabi <tuyen.gabi@gmail.com>
2019-04-14 23:25:56 +0200

    finish js

--- js/script.js
+++ js/script.js
@@ -3,17 +3,16 @@
 // let maxScore = document.getElementsByClassName('maxScore')[0].innerHTML; 
 // const scorePlayer1 = document.getElementsByClassName('scorePlayer1')[0].innerHTML;
 // const scorePlayer2 = document.getElementsByClassName('scorePlayer2')[0].innerHTML;
-let valueInput = document.getElementsByClassName('score')[0].value;
-console.log(valueInput);
-console.log(Number(valueInput));
 const btnPlayer1 = document.getElementsByClassName('btnPlayer1')[0];
 const btnPlayer2 = document.getElementsByClassName('btnPlayer2')[0];
 const btnReset = document.getElementsByClassName('btnReset')[0];
 let score1 = 0;
 let score2 = 0;
 // let max = Number(maxScore);
+let valueInput;
 
 function addMaxScore() {
+ valueInput = document.getElementsByClassName('score')[0].value;
   document.getElementsByClassName('maxScore')[0].innerHTML = valueInput;
 }
 
